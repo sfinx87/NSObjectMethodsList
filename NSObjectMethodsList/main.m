e75359e628956471afea949a9b742dfaeaa08034
Alex <alex@zver.local>
2012-01-09 14:51:42 +0200

    Initial Commit

---
+++ NSObjectMethodsList/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSObjectMethodsList
+//
+//  Created by Alex on 1/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
